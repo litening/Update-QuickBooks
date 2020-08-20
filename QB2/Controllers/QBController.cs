b80f0026858d8d7d2ef38e0f2e30e7c8cf7f7c6c
Balveer <balveer.techfreak@gmail.com>
2020-08-20 16:20:34 +0530

    Initial setup of QB OAuth

---
+++ QB2/Controllers/QBController.cs
@@ -0,0 +1,25 @@
+﻿using Intuit.Ipp.OAuth2PlatformClient;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace QB2.Controllers
+{
+    public class QBController : Controller
+    {
+        public static string clientId = ConfigurationManager.AppSettings["clientid"];
+        public static string clientSecret = ConfigurationManager.AppSettings["clientsecret"];
+        public static string redirectUrl = ConfigurationManager.AppSettings["redirectUrl"];
+        public static string environment = ConfigurationManager.AppSettings["appEnvironment"];
+
+        public static OAuth2Client auth2Client = new OAuth2Client(clientId, clientSecret, redirectUrl, environment);
+
+        public ActionResult InitiateAuth()
+        {
+            List<OidcScopes> scopes = new List<OidcScopes>();
+            scopes.Add(OidcScopes.Accounting);
+            string authorizeUrl = auth2Client.GetAuthorizationURL(scopes);
+            return Redirect(authorizeUrl);
+        }
+    }
+}
